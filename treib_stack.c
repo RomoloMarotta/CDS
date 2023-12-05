cb4a45aff8e76e41505d1fab174f6940e75dfd68
Romolo Marotta <romolo.marotta@gmail.com>
2023-12-05 15:06:45 +0000

    added dummy allocator for treiber
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- treib_stack.c
+++ treib_stack.c
@@ -2,7 +2,15 @@
 #include "stack.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "clock_constant.h"
 
+#define UNROLL 100
+#define ITERS  0
+
+__thread int   local_allocs = 1;
+__thread Node* local_ptr = NULL;
+__thread int initr = 0;
+__thread struct drand48_data randBuffer;
 
 void init(Stack* stack, int n) {
     stack->top = NULL;
@@ -16,28 +24,50 @@ int isEmpty(Stack* stack) {
     return stack->top == NULL;
 }
 
+void backoff(int cnt, int max){
+        if(cnt && ITERS){
+           unsigned long long wait = CLOCK_READ();
+           long r = 0;   lrand48_r(&randBuffer, &r);
+           r = r % ( max << (cnt+1));
+           r = CLOCKS_PER_US*r;
+           while((CLOCK_READ()-wait) < r);
+        }
+}
+
+
 void push(Stack* stack, int value) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) {
+    Node* newNode;
+    local_allocs--;
+    if(!initr) {initr=1;srand48_r(time(NULL), &randBuffer);}
+
+    if(!local_allocs){
+      local_ptr = (Node*)malloc(sizeof(Node)*UNROLL);
+      if (local_ptr == NULL) {
         perror("Error allocating memory");
         exit(EXIT_FAILURE);
+      }
+      local_allocs = UNROLL-1;
     }
-
+    newNode = &local_ptr[local_allocs];
     newNode->data = value;
-
+    int cnt = 0;
     do {
+	backoff(cnt, ITERS);
+        cnt++;
         newNode->next = stack->top;
     } while (!__sync_bool_compare_and_swap(&stack->top, newNode->next, newNode));
 }
 
 int pop(Stack* stack) {
     Node* topNode;
-
+    if(!initr) {initr=1;srand48_r(time(NULL), &randBuffer);}
+    int cnt = 0;
     do {
         topNode = stack->top;
         if (topNode == NULL) {
             return -1; // Stack is empty
         }
+        backoff(cnt, ITERS);
     } while (!__sync_bool_compare_and_swap(&stack->top, topNode, topNode->next));
 
     int value = topNode->data;
